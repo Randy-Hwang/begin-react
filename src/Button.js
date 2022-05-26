98c27a2bff1f1c36b046bb982423a41df1bb3c83
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-05-26 21:04:27 +0900

    second commit

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import styles from "./Button.modules.css";
+
+function Button({ text }) {
+  return <button className={styles.title}>{text}</button>;
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Button;
