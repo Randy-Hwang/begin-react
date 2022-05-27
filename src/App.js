f168f6d4a5f135ff6144788f19cdc31fd162785b
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-05-27 15:39:49 +0900

    Understanding React...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => setCoins(json));
+    setLoading(false);
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome to React!</h1>
-      <Button text={"Continue"} />
+      <h1>The Coins! {coins.length}</h1>
+      {loading ? <strong>LOADING...</strong> : null}
+      <ul>
+        {coins.map((item) => (
+          <li>
+            {item.name} ({item.symbol}) : {item.quotes.USD.price} USD{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
